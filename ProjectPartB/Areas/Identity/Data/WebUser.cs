0a1fc78fc1566a9918296b0638e15ddeb3518b6a
Cao Phan <phan73@manukaumail.com>
2023-10-27 17:04:57 +1300

    Add project files.

---
+++ ProjectPartB/Areas/Identity/Data/WebUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace ProjectPartB.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the WebUser class
+public class WebUser : IdentityUser
+{
+    public string FullName { get; set; }
+    public string Address { get; set; }
+    public string Phone { get; set; }
+    public string DrivingLicense { get; set; }
+    public string UserID { get; set; }
+}
+
+
