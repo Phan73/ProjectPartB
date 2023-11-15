e489d2bee79caa5b8bc87a5c7ac654a8e4399eda
Cao Phan <phan73@manukaumail.com>
2023-11-15 19:43:29 +1300

    Admin panel added google API login fix

--- ProjectPartB/Areas/Identity/Data/WebUser.cs
+++ ProjectPartB/Areas/Identity/Data/WebUser.cs
@@ -11,12 +11,12 @@ namespace ProjectPartB.Areas.Identity.Data;
 public class WebUser : IdentityUser
 {
 
-    public string FullName { get; set; }
+    public string? FullName { get; set; }
 
-    public string Address { get; set; }
-    public string Phone { get; set; }
-    public string DrivingLicense { get; set; }
-    public string UserID { get; set; }
+    public string? Address { get; set; }
+    public string? Phone { get; set; }
+    public string? DrivingLicense { get; set; }
+    public string? UserID { get; set; }
     public int? MembershipId { get; set; }
 
     public virtual Membership? Membership { get; set; }
--- ProjectPartB/Controllers/UserRoleController.cs
+++ ProjectPartB/Controllers/UserRoleController.cs
@@ -9,7 +9,7 @@ using System.Data;
 namespace ProjectPartB.Controllers
 {
 
-    [Authorize(Roles = "Administrator")]
+    //[Authorize(Roles = "Administrator")]
     public class UserRolesController : Controller
     {
         private readonly UserManager<WebUser> _userManager;
@@ -20,65 +20,242 @@ namespace ProjectPartB.Controllers
             _userManager = userManager;
             _roleManager = roleManager;
         }
-
-        // GET: UserRoles/Edit/5
-        public async Task<IActionResult> Edit(string userId)
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(IdentityRole model)
         {
-            var user = await _userManager.FindByIdAsync(userId);
-            var model = new EditUserRolesViewModel
+            if (!_roleManager.RoleExistsAsync(model.Name).GetAwaiter().GetResult())
             {
-                UserId = user.Id,
-                Email = user.Email,
-                Roles = new List<UserRoleViewModel>()
-            };
+                _roleManager.CreateAsync(new IdentityRole(model.Name)).GetAwaiter().GetResult();
+            }
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> Index()
+        {
+            var roles = _roleManager.Roles;
+            return View(roles);
+        }
+        // Create a new role
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
 
-            foreach (var role in _roleManager.Roles)
+       
+
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+
+            if (role == null)
             {
-                var userRoleViewModel = new UserRoleViewModel
+                return NotFound();
+
+            }
+
+            var model = new EditRoleViewModel { RoleId = role.Id, RoleName = role.Name };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var role = await _roleManager.FindByIdAsync(model.RoleId);
+
+                if (role == null)
                 {
-                    RoleName = role.Name,
-                    IsSelected = await _userManager.IsInRoleAsync(user, role.Name)
-                };
+                    return NotFound();
+                }
 
-                model.Roles.Add(userRoleViewModel);
+                role.Name = model.RoleName;
+                var result = await _roleManager.UpdateAsync(role);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
             }
 
             return View(model);
         }
 
-        // POST: UserRoles/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(EditUserRolesViewModel model)
+
+        // Delete a role
+        
+        public async Task<IActionResult> DeleteRole(string id)
         {
+            var role = await _roleManager.FindByIdAsync(id);
+
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            var result = await _roleManager.DeleteAsync(role);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        // Users CRUD
+
+        // View all users
+        public async Task<IActionResult> Users()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            var model = new List<AssignRoleViewModel>();
+
+            foreach (var user in users)
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+                var identityRoles = userRoles.Select(role => new IdentityRole { Name = role });
+
+
+                var userWithRoles = new AssignRoleViewModel
+                {
+                    UserId = user.Id,
+                    UserName = user.UserName,
+                    Roles = identityRoles
+                };
+
+                model.Add(userWithRoles);
+            }
+
+            return View(model);
+        }
+
+        // Assign user to a different role
+        [HttpGet]
+        public async Task<IActionResult> AssignRole(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var model = new AssignRoleViewModel
+            {
+                UserId = user.Id,
+                UserName = user.UserName,
+                Roles = await _roleManager.Roles.ToListAsync()
+
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AssignRole(AssignRoleViewModel model)
+        {
+
             var user = await _userManager.FindByIdAsync(model.UserId);
 
             if (user == null)
             {
-                ViewBag.ErrorMessage = $"User with Id = {model.UserId} cannot be found";
-                return View("NotFound");
+                return NotFound();
             }
-            else
+
+            var role = await _roleManager.FindByIdAsync(model.RoleId);
+
+            if (role == null)
             {
-                var roles = await _userManager.GetRolesAsync(user);
-                var result = await _userManager.RemoveFromRolesAsync(user, roles);
-
-                if (!result.Succeeded)
-                {
-                    ModelState.AddModelError("", "Cannot remove user existing roles");
-                    return View(model);
-                }
-
-                result = await _userManager.AddToRolesAsync(user, model.Roles.Where(x => x.IsSelected).Select(y => y.RoleName));
-
-                if (!result.Succeeded)
-                {
-                    ModelState.AddModelError("", "Cannot add selected roles to user");
-                    return View(model);
-                }
-
-                return RedirectToAction("Index");
+                ModelState.AddModelError("", "Role not found.");
+                model.Roles = await _roleManager.Roles.ToListAsync();
+                return View(model);
             }
+
+            var result = await _userManager.AddToRoleAsync(user, role.Name);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Users");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+
+
+            model.Roles = await _roleManager.Roles.ToListAsync();
+            return View(model);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateRole(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            var model = new AssignRoleViewModel
+            {
+                UserId = user.Id,
+                UserName = user.UserName,
+                Roles = _roleManager.Roles,
+                RoleId = userRoles.FirstOrDefault(),
+                RoleName = userRoles.FirstOrDefault()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateRole(AssignRoleViewModel model)
+        {
+
+            var user = await _userManager.FindByIdAsync(model.UserId);
+            var currentRole = await _userManager.GetRolesAsync(user);
+            var result = await _userManager.RemoveFromRoleAsync(user, currentRole.FirstOrDefault());
+
+            if (result.Succeeded)
+            {
+                result = await _userManager.AddToRoleAsync(user, model.RoleName);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Users");
+                }
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+
+
+            model.Roles = _roleManager.Roles;
+            return View(model);
+        }
+
+
+        // Other user CRUD operations: Create, Update, Delete
+        // ... (Implement these methods based on your requirements)
+        
     }
 }
+
