56fbbaae41c6dc1d8158eb8fd23e7adec0bca773
Phan73 <115679473+Phan73@users.noreply.github.com>
2025-08-08 17:48:42 +1200

    reupload

--- ProjectPartB/Models/CarDescription.cs
+++ ProjectPartB/Models/CarDescription.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjectPartB.Models
 {
@@ -13,13 +14,22 @@ namespace ProjectPartB.Models
 
         public int CarDescriptionId { get; set; }
         public int? CarTypeId { get; set; }
+        [Required(ErrorMessage = "The description is required.")]
+        [StringLength(500, ErrorMessage = "The description cannot be longer than 500 characters.")]
         public string? Description { get; set; }
+        [Required(ErrorMessage = "The brand is required.")]
         public string? Brand { get; set; }
+        [Required(ErrorMessage = "The model is required.")]
         public string? Model { get; set; }
+        [Required(ErrorMessage = "The color is required.")]
         public string? Color { get; set; }
+        [Required(ErrorMessage = "The rate per day is required.")]
+        [Range(0, 10000, ErrorMessage = "The rate per day must be between 0 and 10000.")]
         public decimal RatePerDay { get; set; }
         public bool? Available { get; set; }
+        [Required(ErrorMessage = "Please upload an image.")]
         public string? Image { get; set; }
+        [Required(ErrorMessage = "Please select the car type.")]
         public string CarTypeName { get; set; }
         public virtual CarType? CarType { get; set; }
         public virtual ICollection<CarAvailability> CarAvailabilities { get; set; }
